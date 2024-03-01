f7bb989fc2fda95a11b59aa0bce4a234cd525d10
Ahmed Elsharkawy <shar40951@gmail.com>
2024-03-01 23:53:24 +0200

    Update dependencies and add Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'Features/splash/presentaion/views/splash_view.dart';
 
@@ -15,7 +16,10 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaryColor),
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kprimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       home: const SplashView(),
     );
   }
